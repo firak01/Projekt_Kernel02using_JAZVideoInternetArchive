7daaaf7983208712a5bbd5afeca358e4f350fb93
Firak01 <firak01@t-online.de>
2017-03-16 11:43:39 +0100

    FlagZ eingebaut Signed-off-by: Firak01 <firak01@t-online.de>

--- JAZVideoInternetArchive/src/use/via/client/PanelDlgIPExternalContentVIA.java
+++ JAZVideoInternetArchive/src/use/via/client/PanelDlgIPExternalContentVIA.java
@@ -3,35 +3,30 @@
  */
 package use.via.client;
 
+
 import java.awt.event.ActionEvent;
-import java.util.HashMap;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
 import use.via.client.module.ip.ProgramIPContentVIA;
-
-
+import basic.zBasic.ExceptionZZZ;
+import basic.zBasic.IObjectZZZ;
+import basic.zBasic.util.log.ReportLogZZZ;
+import basic.zBasicUI.thread.SwingWorker;
+import basic.zKernel.IKernelUserZZZ;
+import basic.zKernel.KernelZZZ;
+import basic.zKernelUI.component.KernelActionCascadedZZZ;
+import basic.zKernelUI.component.KernelJDialogExtendedZZZ;
+import basic.zKernelUI.component.KernelJPanelCascadedZZZ;
 
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
-import basic.zBasic.ExceptionZZZ;
-import basic.zBasic.IConstantZZZ;
-import basic.zBasic.IFunctionZZZ;
-import basic.zBasic.IObjectZZZ;
-import basic.zBasic.ReflectCodeZZZ;
-import basic.zBasic.util.log.ReportLogZZZ;
-import basic.zBasicUI.thread.SwingWorker;
-import basic.zKernel.IKernelUserZZZ;
-import basic.zKernelUI.component.KernelActionCascadedZZZ;
-import basic.zKernelUI.component.KernelJDialogExtendedZZZ;
-import basic.zKernelUI.component.KernelJPanelCascadedZZZ;
-import basic.zKernel.KernelZZZ;
 import custom.zKernel.LogZZZ;
 
 /**Das Panel, was im "BorderLayout.CENTER" des entprechenden Dialogs angezeigt werden soll.
@@ -120,7 +115,7 @@ public class PanelDlgIPExternalContentVIA  extends KernelJPanelCascadedZZZ{
 			public void actionPerformPostCustom(ActionEvent ae, boolean bQueryResult) throws ExceptionZZZ {
 			}			 							
 			
-			class SwingWorker4ProgramIPContentVIA extends SwingWorker implements IConstantZZZ, IObjectZZZ, IFunctionZZZ, IKernelUserZZZ{
+			class SwingWorker4ProgramIPContentVIA extends SwingWorker implements IObjectZZZ, IKernelUserZZZ{
 				private KernelZZZ objKernel;
 				private LogZZZ objLog;
 				private KernelJPanelCascadedZZZ panel;
@@ -128,6 +123,9 @@ public class PanelDlgIPExternalContentVIA  extends KernelJPanelCascadedZZZ{
 				
 				private String sText2Update;    //Der Wert, der ins Label geschreiben werden soll. Jier als Variable, damit die intene Runner-Klasse darauf zugreifen kann.
 															// Auch: Dieser Wert wird aus dem Web ausgelesen und danach in das Label des Panels geschrieben.
+				
+							
+				protected ExceptionZZZ objException = null;    // diese Exception hat jedes Objekt
 				
 				public SwingWorker4ProgramIPContentVIA(KernelZZZ objKernel, KernelJPanelCascadedZZZ panel, String[] saFlag4Program){
 					super();
@@ -184,29 +182,6 @@ public class PanelDlgIPExternalContentVIA  extends KernelJPanelCascadedZZZ{
 					//textField.setText("Lese aktuellen Wert .....");
 					
 				}
-				
-				
-				///#### Interfaces
-				public ExceptionZZZ getExceptionObject() {
-					// TODO Auto-generated method stub
-					return null;
-				}
-				public void setExceptionObject(ExceptionZZZ objException) {
-					// TODO Auto-generated method stub
-					
-				}
-				public boolean getFlag(String sFlagName) {
-					// TODO Auto-generated method stub
-					return false;
-				}
-				public boolean setFlag(String sFlagName, boolean bValue) {
-					// TODO Auto-generated method stub
-					return false;
-				}
-				public boolean proofFlagExists(String sFlagName) {
-					// TODO Auto-generated method stub
-					return false;
-				}
 
 				public KernelZZZ getKernelObject() {
 					return this.objKernel;
@@ -223,32 +198,34 @@ public class PanelDlgIPExternalContentVIA  extends KernelJPanelCascadedZZZ{
 				public void setLogObject(LogZZZ objLog) {
 					this.objLog = objLog;
 				}
-
-				@Override
-				public HashMap<String, Boolean> getHashMapFlagZ() {
-					// TODO Auto-generated method stub
-					return null;
+				
+				
+				
+				
+				/* (non-Javadoc)
+				 * @see zzzKernel.basic.KernelAssetObjectZZZ#getExceptionObject()
+				 */
+				public ExceptionZZZ getExceptionObject() {
+					return this.objException;
+				}
+				/* (non-Javadoc)
+				 * @see zzzKernel.basic.KernelAssetObjectZZZ#setExceptionObject(zzzKernel.custom.ExceptionZZZ)
+				 */
+				public void setExceptionObject(ExceptionZZZ objException) {
+					this.objException = objException;
+				}
+				
+				
+				/**Overwritten and using an object of jakarta.commons.lang
+				 * to create this string using reflection. 
+				 * Remark: this is not yet formated. A style class is available in jakarta.commons.lang. 
+				 */
+				public String toString(){
+					String sReturn = "";
+					sReturn = ReflectionToStringBuilder.toString(this);
+					return sReturn;
 				}
 
-				//201702: Enum FLAGZ nutzen 
-				@Override
-				public boolean proofFlagZExists(String sFlagName) {
-					// TODO Auto-generated method stub
-					return false;
-				}
-
-				@Override
-				public boolean setFlagZ(String sFlagName, boolean bFlagValue)
-						throws ExceptionZZZ {
-					// TODO Auto-generated method stub
-					return false;
-				}
-
-				@Override
-				public boolean getFlagZ(String sFlagName) {
-					// TODO Auto-generated method stub
-					return false;
-				}					
 			} //End Class MySwingWorker
 
 			public void actionPerformCustomOnError(ActionEvent ae, ExceptionZZZ ez) {
--- JAZVideoInternetArchive/src/use/via/client/module/export/SwingWorker4ProgramTreeContentVIA.java
+++ JAZVideoInternetArchive/src/use/via/client/module/export/SwingWorker4ProgramTreeContentVIA.java
@@ -3,7 +3,13 @@
  */
 package use.via.client.module.export;
 
+import static java.lang.System.out;
+
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JLabel;
@@ -13,13 +19,20 @@ import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
 import basic.zKernel.KernelZZZ;
 import custom.zKernel.LogZZZ;
 import basic.zBasic.ExceptionZZZ;
 import basic.zBasic.IConstantZZZ;
+import basic.zBasic.IFlagZZZ;
 import basic.zBasic.IFunctionZZZ;
 import basic.zBasic.IObjectZZZ;
+import basic.zBasic.ObjectZZZ;
+import basic.zBasic.ReflectClassZZZ;
 import basic.zBasic.ReflectCodeZZZ;
+import basic.zBasic.IFlagZZZ.FLAGZ;
+import basic.zBasic.util.datatype.string.StringZZZ;
 import basic.zBasic.util.log.ReportLogZZZ;
 import basic.zBasicUI.thread.SwingWorker;
 import basic.zKernel.IKernelUserZZZ;
@@ -32,7 +45,7 @@ import basic.zKernelUI.component.model.JTree.ModelJTreeNodeRootDummyZZZ;
  * @author 0823
  *
  */
-public class SwingWorker4ProgramTreeContentVIA extends SwingWorker implements IConstantZZZ, IObjectZZZ, IFunctionZZZ, IKernelUserZZZ{
+public class SwingWorker4ProgramTreeContentVIA extends SwingWorker implements IObjectZZZ, IFlagZZZ, IKernelUserZZZ{
 	private KernelZZZ objKernel;
 	private LogZZZ objLog;
 	private KernelJPanelCascadedZZZ panelCascaded;
@@ -41,9 +54,13 @@ public class SwingWorker4ProgramTreeContentVIA extends SwingWorker implements IC
 	private String sText2Update; //Der Wert, der mit "update label" in ein Feld geschrieben werden kann, dass �ber den Status "der work" berichten soll.
 	
 	//Flags
+	//TODO: diese einzelvariablen können deaktiviert werden, statt dessen die FLGZ Lösung mit der Hashmap verwednen
 	private boolean bFlagInit = false;
 	private boolean bFlagTerminate = false;
 	private boolean bFlagDebug = false;
+	
+	private HashMap<String, Boolean>hmFlag = new HashMap<String, Boolean>(); //Neu 20130721	
+	protected ExceptionZZZ objException = null;    // diese Exception hat jedes Objekt
 										
 	
 	public SwingWorker4ProgramTreeContentVIA(KernelZZZ objKernel, KernelJPanelCascadedZZZ panelCascaded, String sNewSelection, String[] saFlagControlIn) throws ExceptionZZZ{
@@ -169,11 +186,7 @@ public class SwingWorker4ProgramTreeContentVIA extends SwingWorker implements IC
 	
 	
 	///#### Interfaces
-	public boolean proofFlagExists(String sFlagName) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	
 	public KernelZZZ getKernelObject() {
 		return this.objKernel;
 	}
@@ -188,22 +201,6 @@ public class SwingWorker4ProgramTreeContentVIA extends SwingWorker implements IC
 
 	public void setLogObject(LogZZZ objLog) {
 		this.objLog = objLog;
-	}
-
-	/* (non-Javadoc)
-	 * @see basic.zBasic.IObjectZZZ#getExceptionObject()
-	 */
-	public ExceptionZZZ getExceptionObject() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see basic.zBasic.IObjectZZZ#setExceptionObject(basic.zBasic.ExceptionZZZ)
-	 */
-	public void setExceptionObject(ExceptionZZZ objException) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	public void interrupt(){		
@@ -276,30 +273,321 @@ public class SwingWorker4ProgramTreeContentVIA extends SwingWorker implements IC
 	}
 	
 	//20170308: Enum FLAGZ nutzen
-		@Override
-		public HashMap<String, Boolean> getHashMapFlagZ() {
-			// TODO Auto-generated method stub
-			return null;
+	//### FlagMethods ##########################		
+		public Class getClassFlagZ(){
+			return FLAGZ.class;
 		}
 
-		@Override
-		public boolean proofFlagZExists(String sFlagName) {
-			// TODO Auto-generated method stub
-			return false;
+			public HashMap<String, Boolean>getHashMapFlagZ(){
+				return this.hmFlag;
+			} 
+		
+		/** DIESE METHODE MUSS IN ALLEN KLASSEN VORHANDEN SEIN - über Vererbung -, DIE IHRE FLAGS SETZEN WOLLEN
+		 * Weteire Voraussetzungen:
+		 * - Public Default Konstruktor der Klasse, damit die Klasse instanziiert werden kann.
+		 * - Innere Klassen müssen auch public deklariert werden.
+		 * @param objClassParent
+		 * @param sFlagName
+		 * @param bFlagValue
+		 * @return
+		 * lindhaueradmin, 23.07.2013
+		 */
+		public boolean setFlagZ(String sFlagName, boolean bFlagValue) throws ExceptionZZZ {
+			boolean bFunction = false;
+			main:{
+				if(StringZZZ.isEmpty(sFlagName)) break main;
+				
+
+				bFunction = this.proofFlagZExists(sFlagName);												
+				if(bFunction == true){
+					
+					//Setze das Flag nun in die HashMap
+					HashMap<String, Boolean> hmFlag = this.getHashMapFlagZ();
+					hmFlag.put(sFlagName.toUpperCase(), bFlagValue);
+					bFunction = true;								
+				}										
+			}	// end main:
+			
+			return bFunction;	
 		}
 
-		@Override
-		public boolean setFlagZ(String sFlagName, boolean bFlagValue)
-				throws ExceptionZZZ {
-			// TODO Auto-generated method stub
-			return false;
+		/** DIESE METHODE MUSS IN ALLEN KLASSEN VORHANDEN SEIN - über Vererbung ODER Interface Implementierung -, DIE IHRE FLAGS SETZEN WOLLEN
+		 *  SIE WIRD PER METHOD.INVOKE(....) AUFGERUFEN.
+		 * @param name 
+		 * @param sFlagName
+		 * @return
+		 * lindhaueradmin, 23.07.2013
+		 */
+		public boolean proofFlagZExists(String sFlagName){
+			boolean bReturn = false;
+			main:{
+				if(StringZZZ.isEmpty(sFlagName))break main;
+					System.out.println("sFlagName = " + sFlagName);
+					
+					Class objClass4Enum = this.getClassFlagZ();	//Aufgrund des Interfaces IFlagZZZ wird vorausgesetzt, dass diese Methode vorhanden ist.
+					String sFilterName = objClass4Enum.getSimpleName();
+					
+					ArrayList<Class<?>> listEmbedded = ReflectClassZZZ.getEmbeddedClasses(this.getClass(), sFilterName);
+					if(listEmbedded == null) break main;
+					out.format("%s# ListEmbeddedClasses.size()...%s%n", ReflectCodeZZZ.getPositionCurrent(), listEmbedded.size());
+					
+					for(Class objClass : listEmbedded){
+						out.format("%s# Class...%s%n", ReflectCodeZZZ.getPositionCurrent(), objClass.getName());
+						Field[] fields = objClass.getDeclaredFields();
+						for(Field field : fields){
+							if(!field.isSynthetic()){ //Sonst wird ENUM$VALUES auch zurückgegeben.
+								//out.format("%s# Field...%s%n", ReflectCodeZZZ.getPositionCurrent(), field.getName());
+								if(sFlagName.equalsIgnoreCase(field.getName())){
+									bReturn = true;
+									break main;
+								}
+							}				
+					}//end for
+				}//end for
+					
+				//20170307: Durch das Verschieben von FLAGZ mit den Werten DEBUG und INIT in das IObjectZZZ Interface, muss man explizit auch dort nachsehen.
+			   //                Merke: Das Verschieben ist deshlab notwenig, weil nicht alle Klassen direkt von ObjectZZZ erben können, sondern das Interface implementieren müsssen.
+			
+								
+					//TODO GOON: 
+					//Merke: bReturn = set.contains(sFlagName.toUpperCase());
+					//          Weil das nicht funktioniert meine Util-Klasse erstellen, die dann den String tatsächlich prüfen kann
+					
+					Class<FLAGZ> enumClass = FLAGZ.class;
+					//EnumSet<FLAGZ> set = EnumSet.noneOf(enumClass);//Erstelle ein leeres EnumSet
+					
+					for(Object obj : FLAGZ.class.getEnumConstants()){
+						System.out.println(obj + "; "+obj.getClass().getName());
+						if(sFlagName.equalsIgnoreCase(obj.toString())) {
+							bReturn = true;
+							break main;
+						}
+						//set.add((FLAGZ) obj);
+					}				
+			}//end main:
+			return bReturn;
 		}
 
-		@Override
+	public static boolean proofFlagZExists(IFlagZZZ objcp, String sFlagName) {
+		boolean bReturn = false;
+		main:{
+			if(StringZZZ.isEmpty(sFlagName))break main;
+			if(objcp==null)break main;
+						
+			Class objClass4Enum = objcp.getClassFlagZ();	//Aufgrund des Interfaces IFlagZZZ wird vorausgesetzt, dass diese Methode vorhanden ist.
+			String sFilterName = objClass4Enum.getSimpleName();
+			
+			//Merke: Nachfolgender Code ist Redundant, bleibt aber drin, solange "FLAGZ" nicht irgendwo als Konstante definiert ist.
+			ArrayList<Class<?>> listEmbedded = ReflectClassZZZ.getEmbeddedClasses(objcp.getClass(), sFilterName);
+			if(listEmbedded == null) break main;
+			
+			for(Class objClass : listEmbedded){
+				
+				Field[] fields = objClass.getDeclaredFields();
+				for(Field field : fields){
+					if(!field.isSynthetic()){ //Sonst wird ENUM$VALUES auch zur�ckgegeben.
+						//out.format("%s# Field...%s%n", ReflectCodeZZZ.getPositionCurrent(), field.getName());
+						if(sFlagName.equalsIgnoreCase(field.getName())){
+							bReturn = true;
+							break main;
+						}
+					}				
+				}		
+			}
+		}//End main:
+		if(bReturn){
+			//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# VORHANDEN Flag='" + sFlagName + "'");
+		}else{
+			//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# NICHT DA Flag='" + sFlagName + "'");
+		}
+		return bReturn;
+	}
+
+	public static boolean proofFlagZExists(Class objcp, String sFlagName) {
+		boolean bReturn = false;
+		main:{
+			if(StringZZZ.isEmpty(sFlagName))break main;
+			if(objcp==null)break main;
+								
+			String sFilterName = "FLAGZ"; //TODO: Diesen Namen als Konstante definieren, dann kann auch in diesen static methoden redundanter Code entfernt werden.
+			
+			ArrayList<Class<?>> listEmbedded = ReflectClassZZZ.getEmbeddedClasses(objcp.getClass(), sFilterName);
+			if(listEmbedded == null) break main;
+			
+			for(Class objClass : listEmbedded){
+				
+				Field[] fields = objClass.getDeclaredFields();
+				for(Field field : fields){
+					if(!field.isSynthetic()){ //Sonst wird ENUM$VALUES auch zurückgegeben.
+						out.format("%s# Field...%s%n", ReflectCodeZZZ.getMethodCurrentName(), field.getName());
+						if(sFlagName.equalsIgnoreCase(field.getName())){
+							bReturn = true;
+							break main;
+						}
+					}				
+				}		
+			}
+			
+			//20170307: Durch das Verschieben von FLAGZ mit den Werten DEBUG und INIT in das IObjectZZZ Interface, muss man explizit auch dort nachsehen.
+			   //                Merke: Das Verschieben ist deshlab notwenig, weil nicht alle Klassen direkt von ObjectZZZ erben können, sondern das Interface implementieren müsssen.				
+			Class<FLAGZ> enumClass = FLAGZ.class;				
+			for(Object obj : FLAGZ.class.getEnumConstants()){
+				//System.out.println(obj + "; "+obj.getClass().getName());
+				if(sFlagName.equalsIgnoreCase(obj.toString())) {
+					bReturn = true;
+					break main;
+				}
+			}			
+					
+			//20170308: Merke: Wenn immer noch kein FLAGZ gefunden wurde, dieses Flag aus der aufrufenden Klasse selbst holen.
+			
+		}//End main:
+		if(bReturn){
+			//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# VORHANDEN Flag='" + sFlagName + "'");
+		}else{
+			//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# NICHT VORHANDEN Flag='" + sFlagName + "'");
+		}
+		return bReturn;
+	}
+
+	/* Voraussetzungen:
+	 * - Public Default Konstruktor, damit die Klasse instanziiert werden kann.
+	 * - Innere Klassen müssen auch public deklariert werden.
+	 */
+	public static boolean proofFlagZExists(String sClassName, String sFlagName){
+		boolean bReturn = false;
+		main:{
+			if(StringZZZ.isEmpty(sFlagName))break main;
+			if(StringZZZ.isEmpty(sClassName))break main;
+			try {
+				
+				//Existiert in der Elternklasse oder in der aktuellen Klasse das Flag?
+				//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# ObjektInstanz erzeugen für '" + sClassName + "'");
+				Class<?> objClass = Class.forName(sClassName);		
+				
+				//!!! für abstrakte Klassen gilt: Es kann per Reflection keine neue Objektinstanz geholt werden.
+				if(!ReflectClassZZZ.isAbstract(objClass)){
+					if(ReflectClassZZZ.isInner(objClass)){
+						//Bei inneren Klassen anders eine neue Instanz erzeugen.
+					    //http://stackoverflow.com/questions/17485297/how-to-instantiate-inner-class-with-reflection-in-java
+						Class<?> objClassEnclosing = ReflectClassZZZ.getEnclosingClass(objClass);
+						Object objClassEnclosingInstance = objClassEnclosing.newInstance();
+						
+						try {
+							Constructor<?> ctor= objClass.getDeclaredConstructor(objClassEnclosing);
+							Object objInnerInstance = ctor.newInstance(objClassEnclosingInstance);
+							IFlagZZZ objcp = (IFlagZZZ) objInnerInstance;
+							if(objcp==null){
+							}else{
+								//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# INNERE ObjektInstanz für '" + objcp.getClass().getName() + "' erfolgreich erzeugt. Nun daraus Enum Klasse holen... .");
+								bReturn = ObjectZZZ.proofFlagZExists(objcp, sFlagName);
+							}
+						} catch (SecurityException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (NoSuchMethodException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IllegalArgumentException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InvocationTargetException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}else{
+						IFlagZZZ objcp = (IFlagZZZ)objClass.newInstance();  //Aus der Objektinstanz kann dann gut die Enumeration FLAGZ ausgelesen werden.				
+						if(objcp==null){
+						}else{
+							//System.out.println(ReflectCodeZZZ.getPositionCurrent() + "# ObjektInstanz für '" + objcp.getClass().getName() + "' erfolgreich erzeugt. Nun daraus Enum Klasse holen... .");
+							bReturn = ObjectZZZ.proofFlagZExists(objcp, sFlagName);
+						}
+					}//isInner(...)
+				}else{
+					//System.out.println("Abstrakte Klasse, weiter zur Elternklasse.");
+					Class objcp2 = objClass.getSuperclass();
+					if(objcp2!=null){
+						bReturn = ObjectZZZ.proofFlagZExists(objcp2.getName(), sFlagName);
+					}
+				}
+				
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//20170307: Durch das Verschieben von FLAGZ mit den Werten DEBUG und INIT in das IObjectZZZ Interface, muss man explizit auch dort nachsehen.
+			   //                Merke: Das Verschieben ist deshlab notwenig, weil nicht alle Klassen direkt von ObjectZZZ erben können, sondern das Interface implementieren müsssen.						
+			Class<FLAGZ> enumClass = FLAGZ.class;				
+			for(Object obj : FLAGZ.class.getEnumConstants()){
+				//System.out.println(obj + "; "+obj.getClass().getName());
+				if(sFlagName.equalsIgnoreCase(obj.toString())) {
+					bReturn = true;
+					break main;
+				}
+			}			
+			
+			
+			//20170308: Merke: Wenn immer noch kein FLAGZ gefunden wurde, dieses Flag aus der aufrufenden Klasse selbst holen.
+			
+		}//end main:
+		return bReturn;
+	}
+		
+		/* (non-Javadoc)
+		 * @see zzzKernel.basic.KernelAssetObjectZZZ#getExceptionObject()
+		 */
+		public ExceptionZZZ getExceptionObject() {
+			return this.objException;
+		}
+		/* (non-Javadoc)
+		 * @see zzzKernel.basic.KernelAssetObjectZZZ#setExceptionObject(zzzKernel.custom.ExceptionZZZ)
+		 */
+		public void setExceptionObject(ExceptionZZZ objException) {
+			this.objException = objException;
+		}
+		
+		
+		/**Overwritten and using an object of jakarta.commons.lang
+		 * to create this string using reflection. 
+		 * Remark: this is not yet formated. A style class is available in jakarta.commons.lang. 
+		 */
+		public String toString(){
+			String sReturn = "";
+			sReturn = ReflectionToStringBuilder.toString(this);
+			return sReturn;
+		}
+		
+		/* @see basic.zBasic.IFlagZZZ#getFlagZ(java.lang.String)
+		 * 	 Weteire Voraussetzungen:
+		 * - Public Default Konstruktor der Klasse, damit die Klasse instanziiert werden kann.
+		 * - Innere Klassen m�ssen auch public deklariert werden.(non-Javadoc)
+		 */
 		public boolean getFlagZ(String sFlagName) {
-			// TODO Auto-generated method stub
-			return false;
+			boolean bFunction = false;
+			main:{
+				if(StringZZZ.isEmpty(sFlagName)) break main;
+											
+				HashMap<String, Boolean> hmFlag = this.getHashMapFlagZ();
+				Boolean objBoolean = hmFlag.get(sFlagName.toUpperCase());
+				if(objBoolean==null){
+					bFunction = false;
+				}else{
+					bFunction = objBoolean.booleanValue();
+				}
+								
+			}	// end main:
+			
+			return bFunction;	
 		}
+	
+	
 					
 
 }
