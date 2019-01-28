9b6047efd663c748812f88ce946f453451c88bcc
firak01 <firak01@t-online.de>
2019-01-28 19:13:00 +0100

    Rechschreibung
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZVideoInternetArchive/src/use/via/client/PanelDlgIPExternalContentVIA.java
+++ JAZVideoInternetArchive/src/use/via/client/PanelDlgIPExternalContentVIA.java
@@ -91,7 +91,7 @@ public class PanelDlgIPExternalContentVIA  extends KernelJPanelCascadedZZZ{
 			sIp = objKernel.getParameterByProgramAlias(sModule, sProgram, "IPExternal");			
 		}		
 		
-		//TODO GOON 20190124: Hier soll unterschieden werden zwischen einem absichtlich eingeragenenne Lersstring und nix.
+		//TODO GOON 20190124: Hier soll unterschieden werden zwischen einem absichtlich eingetragenenem Lersstring und nix.
 		if(StringZZZ.isEmpty(sIp)){
 			sIp = "Enter or refresh";
 		}
